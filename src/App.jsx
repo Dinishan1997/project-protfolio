07d082f1b308d602f6e6f5ed8630ff16f25af006
Dinisha <dinishan1997.com>
2024-05-20 14:53:08 -0400

    Initalizing the project Protflio

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      {/* Navigation */}
+      {/* Hero */}
+      {/* Projects */}
+      {/* experience */}
+      {/* footer */}
+    </div>
+  );
+}
+
+export default App;
